a2b1bd830e6f77eea82cd18d3453a5ef49b3e942
Artem Lygin <artemlygin@gmail.com>
2025-06-14 20:34:17 +0300

    Finished Lesson 23
    - Added click on upgrades UI to read
    - Clicking on the ability upgrade item put it into upgrades array

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -19,7 +19,13 @@ func on_level_up(current_level: int):
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
 	add_child(upgrade_screen_instance)
 	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
-	
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
+
+
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	apply_upgrade(upgrade)
+
+
 func apply_upgrade(upgrade: AbilityUpgrade):
 	var has_upgrade = current_upgrades.has(upgrade.upgrade_id)
 	if !has_upgrade:
@@ -30,4 +36,4 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 	else :
 		current_upgrades[upgrade.upgrade_id]["quantity"] += 1
 		
-	# print(current_upgrades)	
+	print(current_upgrades)	
--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -1,7 +1,18 @@
 extends PanelContainer
 
+signal selected
+
 @onready var name_label: Label = $%NameLabel
 @onready var description_label: Label = $%DescriptionLabel
+
+
+func _ready() -> void:
+	gui_input.connect(on_gui_input)
+
+
+func on_gui_input(event: InputEvent):
+	if event.is_action_pressed("lmb_click"):
+		selected.emit()
 
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
