2ebe02641f59ec795bb5da4e1944fd20acc1bbfd
Artem Lygin <artemlygin@gmail.com>
2025-06-16 19:50:05 +0300

    Finished lesson 26
    Added HealthComponent to the Player and make Player to take damage from he enemies in the range

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -3,12 +3,18 @@ extends CharacterBody2D
 const MAX_SPEED = 125
 const ACCELERATION_SMOOTHING: int = 25
 
-# Called when the node enters the scene tree for the first time.
+@onready var damage_interval_timer = $DamageIntervalTimer
+@onready var health_component = $HealthComponent
+@onready var enemies_collision_area = $CollisionArea2D
+
+var number_colliding_bodies: int = 0
+
 func _ready() -> void:
-	pass # Replace with function body.
+	enemies_collision_area.body_entered.connect(on_body_entered)
+	enemies_collision_area.body_exited.connect(on_body_exited)
+	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var movemrnt_vector = get_movement_vector()
 	var direction = movemrnt_vector.normalized()
@@ -22,3 +28,23 @@ func get_movement_vector():
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(x_movement, y_movement)
+
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped(): return
+	health_component.damage(1 * number_colliding_bodies)
+	damage_interval_timer.start()
+	print("❤️: ", health_component.current_health)
+
+
+func on_body_entered(other_body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+	
+	
+func on_body_exited(other_body: Node2D):
+	number_colliding_bodies -= 1
+	
+	
+func on_damage_interval_timer_timeout():
+	check_deal_damage()
