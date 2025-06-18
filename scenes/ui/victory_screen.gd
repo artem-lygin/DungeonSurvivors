4af25f292eadb7e8f0c731a82e2815e5c1c5f8aa
Artem Lygin <artemlygin@gmail.com>
2025-06-18 14:46:09 +0300

    Changed restart scene method

--- scenes/ui/victory_screen.gd
+++ scenes/ui/victory_screen.gd
@@ -12,8 +12,8 @@ func _ready() -> void:
 	
 func on_restart_button_pressed():
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
-	# get_tree().reload_current_scene()
+	# get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+	get_tree().reload_current_scene()
 
 
 func on_quit_button_pressed():
