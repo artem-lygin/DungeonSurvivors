87706233634e00dfe5553c57ec4265e9aabeaf81
Artem Lygin <artemlygin@gmail.com>
2025-06-18 14:37:59 +0300

    Finished Lesson 28
    Added a "Victory" screen on timer timeout with working "Restart" and "Quit" buttons

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,6 +1,18 @@
 extends Node
 
-@onready var timer = $Timer
+@export var victory_screen_scene: PackedScene
+
+@onready var arena_timer: Timer = $%ArenaTimer
+
+
+func _ready() -> void:
+	arena_timer.timeout.connect(on_arena_timer_timeout)
+
 
 func get_time_elapsed():
-	return timer.wait_time - timer.time_left
+	return arena_timer.wait_time - arena_timer.time_left
+
+
+func on_arena_timer_timeout():
+	var victory_screen_instance: Node = victory_screen_scene.instantiate()
+	add_child(victory_screen_instance)
